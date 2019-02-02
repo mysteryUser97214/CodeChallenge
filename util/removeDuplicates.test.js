ce5ee58ef93720023ababded6a970476da7b3b9d
Robin Miller <mitch.furniture123@gmail.com>
2019-02-02 14:48:27 -0800

    create test for duplicate _id

--- util/removeDuplicates.test.js
+++ util/removeDuplicates.test.js
@@ -1,7 +1,39 @@
 import removeDuplicates from './removeDuplicates';
+// import { leads } from '../data/leads.json';
 
 describe('Duplicate removal utility', () => {
-  it('Function should exist', () => {
+  it('Should exist', () => {
     expect(typeof removeDuplicates).toEqual('function');
+  });
+
+  it('Should output a valid object', () => {
+    const result = removeDuplicates({});
+    expect(typeof result).toEqual('object');
+  });
+
+  it('Should remove a record with a duplicate _id', () => {
+    const data = [
+      {
+        _id: '123',
+        email: 'rosie@rosie.com',
+        entryDate: '2014-06-07T17:30:20+00:00',
+      },
+      {
+        _id: '123',
+        email: 'susan@susan.com',
+        entryDate: '2014-05-07T17:30:20+00:00',
+      },
+    ];
+
+    const expected = [
+      {
+        _id: '123',
+        email: 'rosie@rosie.com',
+        entryDate: '2014-06-07T17:30:20+00:00',
+      },
+    ];
+
+    const result = removeDuplicates(data);
+    expect(result).toBe(expected);
   });
 });
